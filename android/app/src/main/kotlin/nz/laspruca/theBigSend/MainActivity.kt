6ad8d0b94aad62bf81356355e0dac5a61bf6135f
Nathan Hare <me@laspruca.nz>
2021-07-20 17:47:04 +1200

    Long boi messages

--- android/app/src/main/kotlin/nz/laspruca/theBigSend/MainActivity.kt
+++ android/app/src/main/kotlin/nz/laspruca/theBigSend/MainActivity.kt
@@ -5,7 +5,6 @@ import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.content.IntentFilter
 import android.telephony.SmsManager
 import android.util.Log
 import android.widget.Toast
@@ -13,7 +12,6 @@ import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
-import java.lang.IllegalArgumentException
 
 
 class MainActivity : FlutterActivity() {
@@ -41,58 +39,53 @@ class MainActivity : FlutterActivity() {
     }
 
     private fun sendSMS(phoneNumber: String, message: String) {
-        val sent = "SMS_SENT"
-        val delivered = "SMS_DELIVERED"
+        val sentPendingIntents = ArrayList<PendingIntent>()
+        val deliveredPendingIntents = ArrayList<PendingIntent>()
+        val sentPI = PendingIntent.getBroadcast(baseContext, 0,
+                Intent(baseContext, SmsSentReceiver::class.java), 0)
 
-        // Create a listener for when the sms messages are sent
-        val sentPI = PendingIntent.getBroadcast(this, 0,
-                Intent(sent), 0)
-
-        // Create a listner for when the SMS
-        val deliveredPI = PendingIntent.getBroadcast(this, 0,
-                Intent(delivered), 0)
-
-        // When the message has been sent
-        registerReceiver(object : BroadcastReceiver() {
-            override fun onReceive(arg0: Context?, arg1: Intent?) {
-                when (resultCode) {
-                    SmsManager.RESULT_ERROR_GENERIC_FAILURE -> {
-                        Toast.makeText(baseContext, "Generic failure sending message",
-                                Toast.LENGTH_SHORT).show()
-                    }
-                    SmsManager.RESULT_ERROR_NO_SERVICE -> Toast.makeText(baseContext, "No service",
-                            Toast.LENGTH_SHORT).show()
-                    SmsManager.RESULT_ERROR_NULL_PDU -> Toast.makeText(baseContext, "Null PDU",
-                            Toast.LENGTH_SHORT).show()
-                    SmsManager.RESULT_ERROR_RADIO_OFF -> Toast.makeText(baseContext, "Radio off",
-                            Toast.LENGTH_SHORT).show()
-                    else -> {
-                    }
-                }
-            }
-        }, IntentFilter(sent))
-
-        // When the sms message was received
-        registerReceiver(object : BroadcastReceiver() {
-            override fun onReceive(arg0: Context?, arg1: Intent?) {
-                when (resultCode) {
-                    Activity.RESULT_OK -> {
-                    }
-                    Activity.RESULT_CANCELED -> Toast.makeText(baseContext, "SMS not delivered",
-                            Toast.LENGTH_SHORT).show()
-                }
-            }
-        }, IntentFilter(delivered))
-
-        // Get the SMSManager to send messages with
-        val sms = SmsManager.getDefault()
+        val deliveredPI = PendingIntent.getBroadcast(baseContext, 0,
+                Intent(baseContext, SmsDeliveredReceiver::class.java), 0)
         try {
-            // Send a message
-            sms.sendTextMessage(phoneNumber, null, message, sentPI, deliveredPI)
-        } catch (ignored: IllegalArgumentException) {
-            // If it did not work
-            Toast.makeText(applicationContext, "Bad phone number $phoneNumber", Toast.LENGTH_LONG).show()
+            val sms = SmsManager.getDefault()
+            val mSMSMessage = sms.divideMessage(message)
+            for (i in 0 until mSMSMessage.size) {
+                sentPendingIntents.add(i, sentPI)
+                deliveredPendingIntents.add(i, deliveredPI)
+            }
+            sms.sendMultipartTextMessage(phoneNumber, null, mSMSMessage,
+                    sentPendingIntents, deliveredPendingIntents)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Toast.makeText(baseContext, "SMS sending failed...",
+                    Toast.LENGTH_SHORT).show()
         }
     }
 
+    private class SmsDeliveredReceiver : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            when (resultCode) {
+                Activity.RESULT_OK -> Toast.makeText(context, "SMS delivered", Toast.LENGTH_SHORT).show()
+                Activity.RESULT_CANCELED -> Toast.makeText(context, "SMS not delivered", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+
+    private class SmsSentReceiver : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            when (resultCode) {
+                RESULT_OK -> Toast.makeText(context,
+                        "SMS Sent" + intent!!.getIntExtra("object", 0),
+                        Toast.LENGTH_SHORT).show()
+                SmsManager.RESULT_ERROR_GENERIC_FAILURE -> Toast.makeText(context, "SMS generic failure", Toast.LENGTH_SHORT)
+                        .show()
+                SmsManager.RESULT_ERROR_NO_SERVICE -> Toast.makeText(context, "SMS no service", Toast.LENGTH_SHORT)
+                        .show()
+                SmsManager.RESULT_ERROR_NULL_PDU -> Toast.makeText(context, "SMS null PDU", Toast.LENGTH_SHORT).show()
+                SmsManager.RESULT_ERROR_RADIO_OFF -> Toast.makeText(context, "SMS radio off", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
 }
